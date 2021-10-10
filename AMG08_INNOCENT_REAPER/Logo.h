5fe5a15964a17eab567f0a308eb284b0d150bfa8
鈴木希海 <121pg0014@tokyo.amg.ac.jp>
2021-10-11 00:54:09 +0900

    Logoクラス：コメント追記中

--- AMG08_INNOCENT_REAPER/Logo.h
+++ AMG08_INNOCENT_REAPER/Logo.h
@@ -1,3 +1,10 @@
+/*****************************************************************//**
+ * \file   Logo.h
+ * \brief  ロゴクラス（画像クラスのサブクラス）
+ * 
+ * \author 鈴木希海
+ * \date   October 2021
+ *********************************************************************/
 #pragma once
 #include "Image.h"
 #include "RedGreenBlue.h"
@@ -5,37 +12,40 @@
 #include <memory>
 
 namespace inr {
-
 	namespace titles {
+		// タイトルアニメーションの画像キー
 		constexpr auto TITLE_ALI = "title_logo_in";
 		constexpr auto TITLE_ALO = "title_logo_out";
 		constexpr auto TITLE_T = "title_animation";
 		constexpr auto TITLE_TLI = "team_logo_in";
 		constexpr auto TITLE_TLO = "team_logo_out";
 	}
-
+	// ロゴクラス
 	class Logo : public Image {
 	public:
 		Logo(Game& game);
 		~Logo() = default;
-
+		// 初期化
 		void Init() override;
+		// 更新
 		void Process() override;
+		// 描画
 		void Draw() override;
 	private:
-		int _number;	// 現在何番目の処理を行っているか？
-		int _fCount;	// フレームカウンタ
-		int _maxFrame;	// 最大再生フレーム
-		int _allnum;	// 画像数
-		int _wait;	// 待ち時間
-		bool _calculation;	// 偽:フェードイン、真:フェードアウト
-		bool _animation;	// アニメーションを行うか否か
-
-		RedGreenBlue _rgb;
-		std::unique_ptr<TitleLogo> _tlogo;
-
-		void ChangeGraph();	// ロゴの切り替え及び、値の初期化(キー入力が有った場合、処理をスキップする)
-		void AnimationInit();	// アニメーションの初期化
+		int _number;						// 現在何番目の処理を行っているか？
+		int _fCount;						// フレームカウンタ
+		int _maxFrame;						// 最大再生フレーム
+		int _allnum;						// 画像数
+		int _wait;							// 待ち時間
+		bool _calculation;					// 偽:フェードイン、真:フェードアウト
+		bool _animation;					// アニメーションを行うか否か
+		RedGreenBlue _rgb;					// RGB
+		std::unique_ptr<TitleLogo> _tlogo;	// タイトルロゴ
+		// ロゴ画像の切り替え
+		void ChangeGraph();
+		// アニメーションの初期化
+		void AnimationInit();
+		// アニメーションのスキップ
 		void AnimaSkip();
 	};
 }
