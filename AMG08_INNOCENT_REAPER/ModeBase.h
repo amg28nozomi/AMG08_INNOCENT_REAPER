354f0aa8e4ed500be7e4166fb7e94879f516f814
鈴木希海 <121pg0014@tokyo.amg.ac.jp>
2021-10-17 05:25:59 +0900

    ModeBase.h：コメントの追記完了

--- AMG08_INNOCENT_REAPER/ModeBase.h
+++ AMG08_INNOCENT_REAPER/ModeBase.h
@@ -1,33 +1,38 @@
 /*****************************************************************//**
- * \file   ModeBase.h
- * \brief  モードのスーパークラス
+ * @file   ModeBase.h
+ * @brief  モードのスーパークラス
  * 
- * \author 鈴木希海
- * \date   October 2021
+ * @author 鈴木希海
+ * @date   October 2021
  *********************************************************************/
 #pragma once
 #include <unordered_map>
 
 namespace inr {
+	/** モードのコンパイル時定数 */
 	namespace mode {
 		// 各種モードのキー情報
 		constexpr auto TITLE = "mode_title";	// タイトル
 		constexpr auto MAIN = "mode_main";		// 本編
 		constexpr auto FIN = "mode_end";		// 終了
 	}
-	// 二重インクルード防止
+	/** 二重インクルード防止 */
 	class Game;
 	class ObjectServer;
 	class ModeServer;
-	// モードベース
+	/** モードベース */
 	class ModeBase {
 	protected:
-		static int _modeFrame;	// 経過フレーム
-		bool _update;			// 処理を更新するかどうか
-		bool _resetFlg;			// モード切り替え時に初期化するかどうか
-		Game& _game;			// ゲームクラス参照
-		std::string _bgmKey;	// bgmのキー
-		// BGMの管理
+		Game& _game;						//!< ゲームクラス参照
+		static int _modeFrame;	//!< 経過フレーム
+		bool _update;						//!< 処理を更新するかどうか
+		bool _resetFlg;					//!< モード切り替え時に初期化するかどうか
+		std::string _bgmKey;		//!< bgmのキー
+		/**
+		 * @brief		BGMの管理
+		 * @return	BGMを停止した場合はtrueを返す
+		 *					BGMを再生した場合はfalseを返す
+		 */
 		bool BgmManage();
 	public:
 		/**
@@ -36,24 +41,43 @@ namespace inr {
 		 */
 		ModeBase(Game& game);
 		/**
-		 * @brief	デストラクタ
+		 * @brief		デストラクタ
 		 */
 		~ModeBase();
-		// 初期化
+		/**
+		 * @brief		初期化処理
+		 */
 		virtual void Init();
-		// 更新
+		/**
+		 * @brief		更新処理
+		 */
 		virtual void Process();	
-		// 描画
+		/**
+		 * @brief		描画処理
+		 */
 		virtual void Draw();
-		// ステージキーの切り替え(引数:次のステージキー)
+		/**
+		 * @brief		ステージキーの切り替え
+		 * @param		次のステージキー
+		 */
 		virtual void ChangeKey(const std::string nextStage);
-		// 初期化フラグのオン
+		/**
+		 * @brief		初期化フラグのオン
+		 */
 		inline void Reset() { _resetFlg = true; }
-		// 経過フレームの初期化
+		/**
+		 * @brief		経過フレームの初期化
+		 */
 		inline void TimeClear() { _modeFrame = 0; };
-		// 経過フレームを取得
+		/**
+		 * @brief		経過フレームを取得
+		 * @return	経過フレームを返す
+		 */
 		inline int GetFrame() { return _modeFrame; }
-		// モードの取得
+		/**
+		 * @brief		キーの取得
+		 * @return	""を返す
+		 */
 		virtual inline std::string GetKey() { return ""; }
 	};
 }
